11fbf6d6ac3aa8322ffb3ac60176b24a754d6bdd
marynachubrei <marinaciubrei@gmail.com>
2021-05-29 13:56:55 +0300

    I added validators for userName and password inputs

--- forms/src/app/reactive-form/reactive-form.component.ts
+++ forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.scss']
 })
-export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ReactiveFormComponent  {
+  form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      userName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(15)
+      ]),
+      password: new FormControl(null, [
+        Validators.required
+      ]),
+      emails: new FormArray([])
+    });
   }
-
 }
