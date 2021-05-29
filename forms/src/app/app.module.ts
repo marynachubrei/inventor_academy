3a1a383b25db0e8f8d0adbbccf463df327c24e38
marynachubrei <marinaciubrei@gmail.com>
2021-05-29 13:10:21 +0300

    I added reactive-form components

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
